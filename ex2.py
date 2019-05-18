8ee2649634d17b5045e1173c771971271b801705
dan <dan932@gmail.com>
2019-05-18 19:19:29 +0300

    added svm and pa

--- ex2.py
+++ ex2.py
@@ -7,9 +7,19 @@ categorical_dict = {
     'I': 2
 }
 
+num_of_classes = 3
+
 
 def category_str_to_int(category):
     return categorical_dict[chr(category[0])]
+
+
+def split_to_validation(x, y, train_data_percentage=0.8):
+    x, y = shuffle_data(x, y)
+    split_index = int(x.shape[0] * train_data_percentage)
+    train_x, train_y, validation_x, validation_y = x[:split_index, :], y[:split_index], \
+                                                   x[split_index:, :], y[split_index:]
+    return train_x, train_y, validation_x, validation_y
 
 
 def load_data(train_x_location, train_y_location, test_x_location, test_y_location):
@@ -21,7 +31,13 @@ def load_data(train_x_location, train_y_location, test_x_location, test_y_locati
     return train_x, train_y, test_x, test_y
 
 
-def one_hot_encode(data, index, num_of_classes):
+def add_bias(x):
+    x_with_bias = np.ones((x.shape[0], x.shape[1] + 1))
+    x_with_bias[:, :-1] = x
+    return x_with_bias
+
+
+def one_hot_encode(data, index):
     one_hot_encoded_data = []
     for row in data:
         binary_encoding = [0] * num_of_classes
@@ -39,12 +55,19 @@ def shuffle_data(x, y):
     return x[p], y[p]
 
 
-def normalize_data(x):
+def normalize_data(x, test_x):
     # z-score normalization
-    x = (x - np.mean(x)) / (np.std(x))
+    # mean = np.mean(x)
+    # std = np.std(x)
+    # x = (x - mean) / std
+    # test_x = (test_x - mean) / std
+
     # min-max normalization
-    # x = (x - np.max(x)) / (np.max(x) - np.min(x))
-    return x
+    min_val = np.min(x)
+    max_val = np.max(x)
+    x = (x - min_val) / (max_val - min_val)
+    test_x = (test_x - min_val) / (max_val - min_val)
+    return x, test_x
 
 
 def precision_calc(w, train_x, train_y):
@@ -53,14 +76,66 @@ def precision_calc(w, train_x, train_y):
         y_hat = np.argmax(np.dot(w, train_x[t]))
         if y_hat != train_y[t]:
             errors += 1
-    print('precision: {0}'.format(1 - (float(errors) / train_x.shape[0])))
+    precision = 1 - (float(errors) / train_x.shape[0])
+    print('train precision: {0}'.format(precision))
+    return precision
+
+
+def pa_train(train_x, train_y):
+    epochs = 100
+    d = train_x.shape[1]
+    w = np.zeros((num_of_classes, d))
+
+    for e in range(epochs):
+        train_x, train_y = shuffle_data(train_x, train_y)
+
+        for x, y in zip(train_x, train_y):
+            y_hat = np.argmax(np.dot(w, x))
+            if y != y_hat:
+                y = int(y)
+                hinge_loss = max(0, 1 - np.dot(w[y], x) + np.dot(w[y_hat], x))
+                tao = hinge_loss / (2 * np.linalg.norm(x))
+                w[y, :] = w[y, :] + tao * x
+                w[y_hat, :] = w[y_hat, :] - tao * x
+
+    precision_calc(w, train_x, train_y)
+    return w
+
+
+def svm_train(train_x, train_y):
+    regularization = 0.1
+    epochs = 100
+    d = train_x.shape[1]
+    eta = 0.001
+    w = np.zeros((num_of_classes, d))
+
+    for e in range(epochs):
+        train_x, train_y = shuffle_data(train_x, train_y)
+
+        for x, y in zip(train_x, train_y):
+            y_hat = int(np.argmax(np.dot(w, x)))
+            y = int(y)
+            if y_hat != y:
+                w[y, :] = (1 - eta * regularization) * w[y, :] + eta * x
+                w[y_hat, :] = (1 - eta * regularization) * w[y_hat, :] - eta * x
+                remaining_row_index = w.shape[0] - (y + y_hat)
+                w[remaining_row_index, :] = (1 - eta * regularization) * w[remaining_row_index, :]
+            else:
+                w[y, :] = (1 - eta * regularization) * w[y, :] + eta * x
+                for i in range(w.shape[0]):
+                    if i != y:
+                        w[i, :] = (1 - eta * regularization) * w[i, :]
+        eta *= 1 - e / epochs
+        # regularization *= 1 - e / epochs
+        # eta /= np.sqrt(e + 1)
+    precision_calc(w, train_x, train_y)
+    return w
 
 
 def perceptron_train(train_x, train_y):
-    num_of_classes = 3
-    epochs = 100000
+    epochs = 100
     d = train_x.shape[1]
-    eta = 0.001
+    eta = 0.01
     w = np.zeros((num_of_classes, d))
 
     for e in range(epochs):
@@ -73,29 +148,43 @@ def perceptron_train(train_x, train_y):
                 w[y, :] = w[y, :] + eta * x
                 w[y_hat, :] = w[y_hat, :] - eta * x
 
+        eta *= 1 - e / epochs
+        # eta /= np.sqrt(e + 1)
     precision_calc(w, train_x, train_y)
     return w
 
 
-def perceptron_predict(w, x, y):
+def predict(w, x, y):
     errors = 0
     for t in range(x.shape[0]):
         y_hat = np.argmax(np.dot(w, x[t]))
         if y_hat != y[t]:
             errors += 1
-    print('precision: {0}'.format(1 - (float(errors) / x.shape[0])))
+    print('test precision: {0}'.format(1 - (float(errors) / x.shape[0])))
 
 
 def main():
     train_x, train_y, test_x, test_y = load_data(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-    train_x = one_hot_encode(train_x, 0, 3)
-    train_x = normalize_data(train_x)
-    test_x = one_hot_encode(test_x, 0, 3)
-    test_x = normalize_data(test_x)
-    # from sklearn.preprocessing import minmax_scale
-    # p = minmax_scale(train_x)
+
+    train_x = one_hot_encode(train_x, 0)
+    test_x = one_hot_encode(test_x, 0)
+    train_x, test_x = normalize_data(train_x, test_x)
+    # train_x = add_bias(train_x)
+    # test_x = add_bias(test_x)
+    train_x, train_y, validation_x, validation_y = split_to_validation(train_x, train_y)
+    # test_x = normalize_data(test_x)
+    print('perceptron:')
     w = perceptron_train(train_x, train_y)
-    perceptron_predict(w, test_x, test_y)
+    predict(w, validation_x, validation_y)
+    predict(w, test_x, test_y)
+    print('svm:')
+    w = svm_train(train_x, train_y)
+    predict(w, validation_x, validation_y)
+    predict(w, test_x, test_y)
+    print('pa:')
+    w = pa_train(train_x, train_y)
+    predict(w, validation_x, validation_y)
+    predict(w, test_x, test_y)
 
 
 if __name__ == "__main__":
